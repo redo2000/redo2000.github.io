16f2e89cb331903e4ba99c6f1efaf5bb0970463c
Redhouane TCHIKOU <64205460+redo2000@users.noreply.github.com>
2026-08-20 17:14:20 +0100

    commit

--- 2nde/ForceGravi.py
+++
@@ -1,7 +0,0 @@
-# Calcul de la force de gravitation
-G = 6.67e-11
-mA = float(input("Donner la valeur de la masse en kg, mA = "))
-mB = float(input("Donner la valeur de la masse en kg, mB = "))
-d = float(input("Donner la distance (en m) entre A et B d = "))
-F = G * mA * mB / d ** 2
-print("L'intensité de la force F est :",f"{F:.2e}",  "N")
--- Premiere/NiveauxEnergieHydrogene.py
+++
@@ -1,5 +0,0 @@
-# Calcul du niveau d'énergie pour l'atome d'Hydrogène
-n = float(input ("La valeur de n = "))
-E = -13.6/n**2
-print ()
-print ("L'énergie est :",round(E,2),"ev"," pour n = ",n)
