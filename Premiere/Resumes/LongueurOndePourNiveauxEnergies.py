e754eaa69fe919dc9881733d9d180eaad35e6ad8
Redhouane TCHIKOU <64205460+redo2000@users.noreply.github.com>
2026-08-20 20:49:52 +0100

    Add files via upload

---
+++ Premiere/Resumes/LongueurOndePourNiveauxEnergies.py
@@ -0,0 +1,5 @@
+# Calcul de la variation d'énergie pour des niveaux d'énergies d'un atome
+e = float(input ("La valeur de la variation d'énergie en ev   "))
+l = 1243/e
+print ()
+print ("La longueur d'onde en nm est :", round(l,2))
---
+++ Premiere/Resumes/NiveauxEnergieHydrogene.py
@@ -0,0 +1,5 @@
+# Calcul du niveau d'énergie pour l'atome d'Hydrogène
+n = float(input ("La valeur de n = "))
+E = -13.6/n**2
+print ()
+print ("L'énergie est :",round(E,2),"ev"," pour n = ",n)
